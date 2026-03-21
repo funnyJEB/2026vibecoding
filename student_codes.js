aec60b6f56ed35bec9373c799e49fcb35479c2ea
Eunbae Jeong <111253985+funnyJEB@users.noreply.github.com>
2026-03-21 16:53:39 +0900

    Add student data with HTML code snippets

---
+++ student_codes.js
@@ -0,0 +1,17 @@
+// student_codes.js
+const studentData = [
+  {
+    id: "01",
+    name: "김철수",
+    appName: "화성 탐사 로봇 시뮬레이터",
+    // 아래 code 부분에 추출한 HTML 전체 코드를 백틱(``)으로 감싸서 넣습니다.
+    code: `<!DOCTYPE html><html>... (철수의 html 코드 전체) ...</html>`
+  },
+  {
+    id: "02",
+    name: "이영희",
+    appName: "나만의 원소 주기율표",
+    code: `<!DOCTYPE html><html>... (영희의 html 코드 전체) ...</html>`
+  },
+  // ... 나머지 학생들 추가
+];
